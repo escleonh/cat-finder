7b9ee76039a7dc282fc1e7c7c6486d82592b80dc
escleonh <hescalante@atu.edu>
2021-07-08 12:33:36 -0500

    slider-menu for mobile devices added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,13 @@
 import './Navbar.css';
 const Navbar = () => {
+
+    const handleSlidebar = ()=>{
+        console.log("CLIKE");
+        const slidebar = document.querySelector('.slidebar');
+        slidebar.classList.toggle('slidebarShow');
+        // slidebar.style.transform = "translateX(-100%)";
+    };
+
     return ( <nav className="Navbar">
        
         <ul className="nav-menu">
@@ -7,6 +15,18 @@ const Navbar = () => {
             <li><a href="/">HOME</a></li>
             <li><a href="/cats">CATS</a></li>
         </ul>
+        <div className="menu-Burger" onClick={()=>handleSlidebar()}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="slidebar">
+        <ul className="slidebar-menu">
+            <li><a href="/">HOME</a></li>
+            <li><a href="/cats">CATS</a></li>
+        </ul>
+        </div>
+       
     </nav> );
 }
  
